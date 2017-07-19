45ac486b7a30fd27181c0335aeb070651b8837cc
Andres Huertas S <afhuertass@unal.edu.co>
2017-07-18 22:06:46 -0500

    #

--- util2.py
+++ util2.py
@@ -29,7 +29,7 @@ def human( p , ids  ):
   # we then the index of the
   
   for x in np.arange(0 , p.shape[0]):
-    mask = p[x][:] > 0.6
+    mask = p[x][:] > 0.4
     elements = np.nonzero( mask )
     idd = str(ids[x][0]) + ", "
     
@@ -40,7 +40,7 @@ def human( p , ids  ):
     if resp == "":
       resp = "None"
 
-    print ( idd + resp + "\n"  )
+    
     yield (idd + resp + "\n")
   
   
